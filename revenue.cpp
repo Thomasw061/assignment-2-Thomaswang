9a67484642e906a26bef6a62df059526722fc028
Thomas Wang <wangqua@flip3.engr.oregonstate.edu>
2026-04-23 15:20:38 -0700

    revenue code

--- revenue.cpp
+++ revenue.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double get_number(){
+	double num;
+	cout << "Enter number one by one cost per, base cost, revenue per" << endl;
+	cin >> num;
+	return num;
+}
+
+
 int main() {
+	double costp = get_number();
+	double b = get_number();
+	double revp = get_number();
+	double N = (b/(revp-costp));
+
+
+	cout << N << endl;
+
+	return 0;
 
 }
